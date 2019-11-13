4b488381fc555bdc325fd4b208f267da4222a745
RA <ra_lebedev@mail.ru>
2019-11-13 09:25:33 +0600

    2

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
 <?php
+declare();
+# Объявление типа параметра
+# string, int, bool, float,
+function AnyFunction(int ...$params)
+{
+    return array_sum($params);
+}
+
+function arraySum(array ...$arrays): array
+{
+    return array_map(function(array $array): int {
+        return array_sum($array);
+    }, $arrays);
+}
